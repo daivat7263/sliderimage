a82f095902171aa5aef0db5fe19667ce9e772752
COE-02 <coe-02@coe-02s-imac.local>
2019-09-18 14:33:28 +0530

    Change the image through slider and button.

--- sliderimage/ViewController.swift
+++ sliderimage/ViewController.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
+{
+    var Array = [UIImage]()
+    
+    @IBOutlet weak var Imageview: UIImageView!
+   
+    @IBOutlet weak var Slider1: UISlider!
+    
+    @IBAction func Slider(_ sender: UISlider)
+    {
+        var value = Int(sender.value)
+        Imageview.image = Array[value]
+    }
+    @IBAction func prevbutton(_ sender: Any)
+    {
+        Slider1.value -= 1
+        Imageview.image = Array[Int(Slider1.value)]
+    }
+    @IBAction func nextbutton(_ sender: Any)
+    {
+        Slider1.value += 1
+        Imageview.image = Array[Int(Slider1.value)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        Array = [#imageLiteral(resourceName: "no1"),#imageLiteral(resourceName: "no2"),#imageLiteral(resourceName: "no3"),#imageLiteral(resourceName: "no4"),#imageLiteral(resourceName: "no5"),#imageLiteral(resourceName: "no6"),#imageLiteral(resourceName: "no7"),#imageLiteral(resourceName: "no8")]
         // Do any additional setup after loading the view, typically from a nib.
-    }
+}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
